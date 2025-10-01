1a0a451f36dce4eb3bd55327f485e30f0c732ccd
maxone2022 <121789728+maxone2022@users.noreply.github.com>
2025-10-01 09:33:58 +0800

    Update next.config.mjs

--- mgx-art/next.config.mjs
+++ mgx-art/next.config.mjs
@@ -1,1 +1,13 @@
-export default { reactStrictMode: true };
+diff --git a/mgx-art/next.config.mjs b/mgx-art/next.config.mjs
+index 6f8976770495f09cbc5dc29ec45c3f94867556dd..46e10b6ada75d7f8b4e697cb9b1e352a08660503 100644
+--- a/mgx-art/next.config.mjs
++++ b/mgx-art/next.config.mjs
+@@ -1 +1,7 @@
+-export default { reactStrictMode: true };
++/** @type {import('next').NextConfig} */
++const nextConfig = {
++  images: { unoptimized: true },
++  reactStrictMode: true,
++};
++
++export default nextConfig;
