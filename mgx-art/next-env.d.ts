84c6d3cb1206e33e2aba17cc0c28aa280b819590
maxone2022 <121789728+maxone2022@users.noreply.github.com>
2025-10-01 09:33:27 +0800

    Update next-env.d.ts

--- mgx-art/next-env.d.ts
+++ mgx-art/next-env.d.ts
@@ -1,3 +1,16 @@
-<reference types="next" />
-<reference types="next/image-types/global" />
-// NOTE: This file should not be edited
+diff --git a/mgx-art/next-env.d.ts b/mgx-art/next-env.d.ts
+index 12076b7d25255b6fa1ab7686110586f25547ded4..2430c48c10bd20bb8f1fd02e7a126ae8bf3600ca 100644
+--- a/mgx-art/next-env.d.ts
++++ b/mgx-art/next-env.d.ts
+@@ -1,3 +1,9 @@
+-<reference types="next" />
+-<reference types="next/image-types/global" />
++/// <reference types="next" />
++/// <reference types="next/image-types/global" />
++
+ // NOTE: This file should not be edited
++declare module "*.svg" {
++  import type { ReactElement, SVGProps } from "react";
++  const content: (props: SVGProps<SVGSVGElement>) => ReactElement;
++  export default content;
++}
